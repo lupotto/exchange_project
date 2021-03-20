44f50f12f689a8baa6588f7210d4a8ef9d004c50
Alex Gracia <alex.gracia.lupotto@gmail.com>
2021-03-20 11:46:40 +0100

    added table for trades

--- src/components/Trades.js
+++ src/components/Trades.js
@@ -4,7 +4,27 @@ import { connect } from 'react-redux'
 class Trades extends Component {
   render(){
     return (
-
+      <div className="card bg-dark text-white">
+        <div className="card-header">
+          Card Title
+        </div>
+        <div className="card-body">
+          <table className="table table-dark table-sm small">
+              <thead>
+                <tr>
+                  <th >Time</th>
+                  <th >ALEX</th>
+                  <th >ALEX/ETH</th>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </thead>
+            </table>
+        </div>
+      </div>
     )
   }
 }
