78346ed9cc279c1a96b9756b4685bd53490b7cf7
Alex Gracia <alex.gracia.lupotto@gmail.com>
2021-03-31 13:51:57 +0200

    with build

---
+++ build/precache-manifest.29bb2ac0235a07f7adffa70737ae6e47.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c24cc966ec817c58fcc3eee792e6ac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f750d0be44cd6694f25e",
+    "url": "./static/css/2.7cc9718e.chunk.css"
+  },
+  {
+    "revision": "f331d856121a79c6402c",
+    "url": "./static/css/main.94086239.chunk.css"
+  },
+  {
+    "revision": "f750d0be44cd6694f25e",
+    "url": "./static/js/2.29aea541.chunk.js"
+  },
+  {
+    "revision": "7fd9a006e1625008af9e7e80bb98b59a",
+    "url": "./static/js/2.29aea541.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0f795a45b20ae6563c4",
+    "url": "./static/js/3.9f0ccb42.chunk.js"
+  },
+  {
+    "revision": "f331d856121a79c6402c",
+    "url": "./static/js/main.680bc7f3.chunk.js"
+  },
+  {
+    "revision": "b9bb0e7230702981e641",
+    "url": "./static/js/runtime-main.71231b67.js"
+  }
+]);
